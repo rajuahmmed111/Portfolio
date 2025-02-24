b18d5f98439d91002fbbe5195214bd5aa0182bd9
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-24 17:36:50 +0600

    added the contact page

--- src/Home/Shared/Contact.jsx
+++ src/Home/Shared/Contact.jsx
@@ -3,22 +3,48 @@ import { IoMdMail } from "react-icons/io";
 import EmailForm from "./EmailForm";
 
 const Contact = () => {
-    return (
-        <div id="contact">
-            <div className=" py-10 md:flex justify-between px-5 md:px-0">
-                <div className="container mx-auto px-5 md:px-20 text-gray-200">
-                    <h2 className="text-4xl text-emerald-400 font-bold pb-4">Contact Information</h2>
-                    <p>Fill in the form to start a conversation</p>
-                    <div className="text-center w-full pt-20 flex text-5xl gap-5 mx-auto"><a href="mailto:nusratjahan3657r@gmail.com" className="text-yellow-600"><IoMdMail></IoMdMail></a>
-                        <a href="https://www.linkedin.com/in/nusrat-jahan-r7/" target="_blank" rel="noopener noreferrer" className="text-blue-700"><FaLinkedin></FaLinkedin></a>
-                        <a href="https://web.facebook.com/afrin.tisha.524934" target="_blank" rel="noopener noreferrer" className="text-blue-500"><FaFacebook></FaFacebook></a>
-                        <a href="https://github.com/nusrat3657" target="_blank" rel="noopener noreferrer" className=""><FaGithub></FaGithub></a>
-                    </div>
-                </div>
-                <EmailForm></EmailForm>
-            </div>
+  return (
+    <div id="contact">
+      <div className=" py-10 md:flex justify-between px-5 md:px-0">
+        <div className="container mx-auto px-5 md:px-20 text-gray-200">
+          <h2 className="text-4xl text-emerald-400 font-bold pb-4">
+            Contact Information
+          </h2>
+          <p>Fill in the form to start a conversation</p>
+          <div className="text-center w-full pt-20 flex text-5xl gap-5 mx-auto">
+            <a href="mailto:ahraju2003@gmail.com" className="text-yellow-600">
+              <IoMdMail></IoMdMail>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/md-raju-ahmed-91830a285/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-700"
+            >
+              <FaLinkedin></FaLinkedin>
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=100033792314596"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500"
+            >
+              <FaFacebook></FaFacebook>
+            </a>
+            <a
+              href="https://github.com/rajuahmmed111"
+              target="_blank"
+              rel="noopener noreferrer"
+              className=""
+            >
+              <FaGithub></FaGithub>
+            </a>
+          </div>
         </div>
-    );
+        <EmailForm></EmailForm>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
