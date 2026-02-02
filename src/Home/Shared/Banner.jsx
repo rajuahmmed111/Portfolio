498f3fc02d6932a68d8ed6b659e0fc90b3d51043
MD RAJU AHMMED <ahraju2003@gmail.com>
2026-02-02 23:24:27 +0600

    update banner

--- src/Home/Shared/Banner.jsx
+++ src/Home/Shared/Banner.jsx
@@ -74,9 +74,9 @@ const Banner = () => {
 
                 {/* download resume */}
                 <a
-                  href="/WebDeveloper_Resume_of_Nusrat.pdf"
+                  href="/RAJU-AHMMED-CV 20.01.2026.pdf"
                   className="btn border-2 border-[#10cc7b] bg-neutral-900 text-white hover:bg-[#10cc7b] hover:text-black hover:border-[#10cc7b]"
-                  download={"WebDeveloper_Resume_of_Nusrat.pdf"}
+                  download={"RAJU-AHMMED-CV 20.01.2026.pdf"}
                 >
                   Download Resume{" "}
                   <span className="text-xl">
